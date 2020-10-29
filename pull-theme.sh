6c5cf2cbf95f3f7b26cf9a3b450cf38a0ea969b0
gregbugaj <gregbugaj@yahoo.com>
2020-10-29 14:15:20 -0500

    WIP

--- pull-theme.sh
+++ pull-theme.sh
@@ -2,4 +2,4 @@
 
 set -e
 echo -e "\033[0;32mUpdating theme...\033[0m"
-git subtree pull --prefix=themes/hugo-theme-learn/ git@github.com:matcornic/hugo-theme-learn.git master --squash
+git subtree pull --prefix=themes/hugo-theme-learn/ https://github.com/matcornic/hugo-theme-learn.git master --squash
