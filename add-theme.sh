07658d7f48836cb351473af3a8518e262c3958c5
gregbugaj <gregbugaj@yahoo.com>
2020-10-29 15:36:19 -0500

    WIP

--- add-theme.sh
+++ add-theme.sh
@@ -2,4 +2,4 @@
 
 set -e
 echo -e "\033[0;32mAdding theme...\033[0m"
-git submodule add https://github.com/matcornic/hugo-theme-learn.git themes/hugo-theme-learn
+git subtree add --prefix=themes/hugo-theme-learn https://github.com/matcornic/hugo-theme-learn.git master --squash
