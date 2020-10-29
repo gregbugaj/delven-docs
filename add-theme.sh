0f39eec9b6e4022fb07135ade07e01d1080530b7
gregbugaj <gregbugaj@yahoo.com>
2020-10-29 06:16:11 -0500

    Test build

---
+++ add-theme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+echo -e "\033[0;32mAdding theme...\033[0m"
+git submodule add git@github.com:matcornic/hugo-theme-learn.git themes/hugo-theme-learn
---
+++ pull-theme.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -e
+echo -e "\033[0;32mUpdating theme...\033[0m"
+git subtree pull --prefix=themes/hugo-theme-learn/ git@github.com:matcornic/hugo-theme-learn.git master --squash
